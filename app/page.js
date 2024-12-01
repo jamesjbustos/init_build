32a4ede6af25daefbbcfbcdf7fd2016303ec6a02
Zaynerrr <zaynlatchman@gmail.com>
2024-11-30 20:09:47 -0500

    Dependicies

---
+++ app/page.js
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Box, Typography } from "@mui/material";
+
+export default function Home() {
+  return (
+    <Box
+    sx={{
+      display: 'flex',
+      alignContent: 'center',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+    >
+      <Typography
+      > </Typography>
+    </Box>
+  );
+}
